e0d7050e29f5b502676f96b3f278c65a2b77f3d8
sea10wood <miyusea5@gmail.com>
2023-05-20 22:38:51 +0900

    Modul-fix

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -3,16 +3,19 @@ import { Box } from "@mui/material";
 // import DataPicker from "../components/DataPicker";
 export default function Todo() {
     return (
-        <Box
-            component="div"
-            sx={{ height: "80vh", width: "40%" }}
-            style={{
-                backgroundColor: "#fffff0",
-                margin: "0 0",
-            }}
-        >
-            {/* <Image src={nyan} alt="nyan.gif" height={150} width={150} /> */}
-            <TodoAdd />
-        </Box>
+        <div>
+            {/* <Threejs /> */}
+            <Box
+                component="div"
+                sx={{ height: "80vh", width: "40%" }}
+                style={{
+                    backgroundColor: "#fffff0",
+                    margin: "0",
+                }}
+            >
+                {/* <Image src={nyan} alt="nyan.gif" height={150} width={150} /> */}
+                <TodoAdd />
+            </Box>
+        </div>
     );
 }
