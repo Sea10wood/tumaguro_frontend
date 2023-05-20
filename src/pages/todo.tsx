dc609b396dadc79c4a0624f064b1be05973ac9aa
tosaken <v-ntosa@alterbooth.net>
2023-05-20 23:28:02 +0900

    add:schedule取得の実装 #20

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,18 +1,22 @@
 import { TodoAdd } from "@/components/Todoadd";
-import { DatePicker } from "@mui/lab";
-import { Box } from "@mui/material";
+import { ScheduleCalendar } from "@/components/schedule/ScheduleCalender";
+import { Box, Stack } from "@mui/material";
 // import DataPicker from "../components/DataPicker";
 export default function Todo() {
     return (
-        <Box
-            component="div"
-            sx={{ height: "100vh", width: "40%" }}
-            style={{
-                backgroundColor: "#fffff0",
-            }}
-        >
-            {/* <Image src={nyan} alt="nyan.gif" height={150} width={150} /> */}
-            <TodoAdd />
-        </Box>
+        <Stack direction="row">
+            <Box component="div" sx={{ height: "100vh", width: "60%" }}></Box>
+            <Box
+                component="div"
+                sx={{ height: "100vh", width: "40%" }}
+                style={{
+                    backgroundColor: "#fffff0",
+                }}
+            >
+                {/* <Image src={nyan} alt="nyan.gif" height={150} width={150} /> */}
+                <ScheduleCalendar />
+                <TodoAdd />
+            </Box>
+        </Stack>
     );
 }
