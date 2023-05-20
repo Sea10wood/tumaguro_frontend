d37a42a23eec7eddcd396c511cee601da3e8016b
sea10wood <miyusea5@gmail.com>
2023-05-20 17:40:53 +0900

    CreateTheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { Layout } from "@/components/common/layout";
+import { createTheme } from "@/libs/Themes";
 import "@/styles/globals.css";
+import { ThemeProvider } from "@emotion/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const theme = createTheme();
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ThemeProvider>
     );
 }
