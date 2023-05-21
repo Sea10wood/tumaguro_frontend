10bbbf5f32bae7ef81c318149c34e11a8361e845
kuga <fkonghe@gmail.com>
2023-05-21 13:49:07 +0900

    make the top page

--- src/components/Signinbutton.tsx
+++ src/components/Signinbutton.tsx
@@ -12,20 +12,23 @@ import { useState } from "react";
 
 export default function SigninButton() {
   return (
-    <Typography style={{ 
-      textAlign: "center",}}>
-    <Box margin="0.1px" component="div" p={2} >
-    <Button
-      sx={{
-        width: "20%",
-        alignItems: "center",
-        margin: "0px",
+    <Typography
+      style={{
+        textAlign: "center",
       }}
-      variant="text"
     >
-      sign in
-    </Button>
-    </Box>
-          </Typography>
+      <Box margin="0.1px" component="div" p={2}>
+        <Button
+          sx={{
+            width: "20%",
+            alignItems: "center",
+            margin: "0px",
+          }}
+          variant="text"
+        >
+          sign in
+        </Button>
+      </Box>
+    </Typography>
   );
 }
--- src/components/Signupbutton.tsx
+++ src/components/Signupbutton.tsx
@@ -9,35 +9,24 @@ import {
 } from "@mui/material";
 import React from "react";
 
-
 import FemaleWorkerCheckingData from "../images/female-worker-checking-data.png";
-
 
 export default function SignupButton() {
   return (
-  
-
-
-
-
-
-
-
-  <Typography style={{ textAlign: "center" }}>
-  <Box component="div" p={2} padding="0" >
-
-    <Button
-     color="primary"
-      sx={{
-        width: "20%",
-        alignItems: "center",    
-        margin: "0px",
-      }}
-      variant="text"
-    >
-      sign up
-    </Button>
-    </Box>
-          </Typography>
+    <Typography style={{ textAlign: "center" }}>
+      <Box component="div" p={2} padding="0">
+        <Button
+          color="primary"
+          sx={{
+            width: "20%",
+            alignItems: "center",
+            margin: "0px",
+          }}
+          variant="text"
+        >
+          sign up
+        </Button>
+      </Box>
+    </Typography>
   );
 }
