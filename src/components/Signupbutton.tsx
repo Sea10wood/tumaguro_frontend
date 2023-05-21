e4fea5d4d6c0153084d416a981d65e496b0da90a
kuga <fkonghe@gmail.com>
2023-05-21 09:12:29 +0900

    fix a sign up page

--- src/components/Signupbutton.tsx
+++ src/components/Signupbutton.tsx
@@ -9,33 +9,23 @@ import {
 } from "@mui/material";
 import React from "react";
 
-
 import FemaleWorkerCheckingData from "../images/female-worker-checking-data.png";
-
 
 export default function SignupButton() {
   return (
-  
-
-
-
-
-
-
-
-  <h2 style={{ textAlign: "center" }}>
-  <Box component="div" p={2}>
-
-    <Button
-     color="primary"
-      sx={{
-        width: "20%",
-        alignItems: "center"      }}
-      variant="text"
-    >
-      sign up
-    </Button>
-    </Box>
-          </h2>
+    <Typography style={{ textAlign: "center" }}>
+      <Box component="div" p={2}>
+        <Button
+          color="primary"
+          sx={{
+            width: "20%",
+            alignItems: "center",
+          }}
+          variant="text"
+        >
+          sign up
+        </Button>
+      </Box>
+    </Typography>
   );
 }
